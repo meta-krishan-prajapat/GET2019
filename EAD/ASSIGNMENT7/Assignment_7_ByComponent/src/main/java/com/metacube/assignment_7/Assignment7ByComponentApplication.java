aa4d4ee7c5810b2ae007e15a49bc6424e7c6af5b
krishan kumar prajapat <krishan.prajapat@metacube.com>
2019-09-11 18:10:37 +0530

    EAD ASSIGNMENT 7

---
+++ EAD/ASSIGNMENT7/Assignment_7_ByComponent/src/main/java/com/metacube/assignment_7/Assignment7ByComponentApplication.java
@@ -0,0 +1,15 @@
+package com.metacube.assignment_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment7ByComponentApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Assignment7ByComponentApplication.class, args);
+	}
+
+}
---
+++ EAD/ASSIGNMENT7/Assignment_7_ByConfig/src/main/java/com/metacube/assignment_7/service/MailSender.java
@@ -0,0 +1,16 @@
+package com.metacube.assignment_7.service;
+
+/**
+ * 
+ * @author Krishan
+ * This is mail sender Interface
+ *
+ */
+public interface MailSender {
+
+	/**
+	 * This method send mail
+	 * @return true if mail send else false
+	 */
+	public boolean sendMail();
+}
