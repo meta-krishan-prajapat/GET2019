4f42a50f0094a37c8dcc13ff318039291c80f292
krishan kumar prajapat <krishan.prajapat@metacube.com>
2019-09-19 19:08:06 +0530

    EAD ASSIGNMENT 9 AND 10

---
+++ EAD/ASSIGNMENT9_10/parking/src/main/java/com/metacube/assignment9_10/ParkingApplication.java
@@ -0,0 +1,19 @@
+package com.metacube.assignment9_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author krishan
+ * ParkingApplication class contains main method
+ */
+@SpringBootApplication
+public class ParkingApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ParkingApplication.class, args);
+	}
+
+}
